62c5bd69913a8eac807869a46a457009fe9052ea
Mehedi Hassan Piash <piash599@gmail.com>
2025-09-15 21:59:35 +0600

    - Fixed API endpoints for upcoming and top-rated movies - Updated iOS deployment target to 13.0 - Updated Flutter SDK constraint to >=3.29.0 - Updated various package dependencies

--- lib/features/movie/data/datasources/movie_remote_data_source.dart
+++ lib/features/movie/data/datasources/movie_remote_data_source.dart
@@ -27,7 +27,7 @@ class MovieRemoteDataSource {
 
   Future<List<MovieModel>> getUpcoming(int page) async {
     final response =
-        await dio.get('movie/top_rated', queryParameters: {'page': page});
+        await dio.get('movie/upcoming', queryParameters: {'page': page});
     return (response.data['results'] as List)
         .map((e) => MovieModel.fromJson(e))
         .toList();
@@ -35,7 +35,7 @@ class MovieRemoteDataSource {
 
   Future<List<MovieModel>> getTopRated(int page) async {
     final response =
-        await dio.get('movie/upcoming', queryParameters: {'page': page});
+        await dio.get('movie/top_rated', queryParameters: {'page': page});
     return (response.data['results'] as List)
         .map((e) => MovieModel.fromJson(e))
         .toList();
